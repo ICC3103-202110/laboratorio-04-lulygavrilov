e2bc44b4f773688ee1809602e072e012a6f0c690
lulygavri <lbgavrilov@miuandes.cl>
2021-05-10 00:00:39 -0400

    ready

--- appGavrilov.js
+++ appGavrilov.js
@@ -1,15 +1,8 @@
-// recibe el valor actual del contador
-//retorna un string con el formato que se muestra
-//en el video Count, (+) (-) (q) for quit, What would you do
+
 function view(counter) {
     return `\n\n Count:  ${counter} \n\n (+) (-) \n\n (q) for quit \n`
 }
 
-//recibe como parametro '+', '-'
-//si msg = '+' retorna counter +1
-//si msg = '-' retorna counter -1
-//funcion pura, solo depende de los parametros que recibe
-//(no cambia ninguna variable externa)
 function update(msg,counter){
     if (msg === '+') return counter+1 ;
     else if (msg === '-') return counter-1 ;
@@ -18,11 +11,6 @@ function update(msg,counter){
 
     }
 
-//brain of the app
-//es impura porque lleva el estado del contador
-//lee el input, llama la funcion anterior en un loop infinito
-//hasta que la app cierre (q)?
-// obtener el valor del contador actual
 function app(counter){
     while (counter !== 'stop'){
         const currentView = view(counter);
@@ -33,7 +21,5 @@ function app(counter){
         counter = update(msg,counter);
         }
 }
-
-//initialize:
 
 app(0);
